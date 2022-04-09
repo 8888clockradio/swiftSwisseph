7d80a46a6c55b93eab7ae686ddef0dbf5a530dd1
88:88 Clock Radio <grosar@georgerosar.com>
2022-04-09 12:06:12 -0500

    swisseph source

---
+++ swiftSwisseph/swiftSwisseph.h
@@ -0,0 +1,17 @@
+//
+//  swiftSwisseph.h
+//  swiftSwisseph
+//
+//  Created by George Rosar on 10/6/20.
+//  Copyright © 2020 George Rosar. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+#import "swephexp.h"
+
+//! Project version number for swiftSwisseph.
+FOUNDATION_EXPORT double swiftSwissephVersionNumber;
+
+//! Project version string for swiftSwisseph.
+FOUNDATION_EXPORT const unsigned char swiftSwissephVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftSwisseph/PublicHeader.h>
